e8f8258b848e6f0349d0d4f8f37d84bdbd7369ab
LêMinhHuy-2280601152 <121075876+Minh-Huyy@users.noreply.github.com>
2025-03-11 11:01:24 +0700

    Delete Lab-02/ex01 directory

--- Lab-02/ex01/lab-01/ex01/hello.py
+++
@@ -1,3 +0,0 @@
-print("Hello, World!")
-print("My name is Hao")
-print("HUTECH University")
--- Lab-02/ex01/lab-01/ex02/ex02_02.py
+++
@@ -1,3 +0,0 @@
-ban_kinh = float(input("Nhap ban kinh cua hinh tron: "))
-dt = 3.14 * (ban_kinh ** 2)
-print("Dien tich cua hinh tron la: ", dt)
