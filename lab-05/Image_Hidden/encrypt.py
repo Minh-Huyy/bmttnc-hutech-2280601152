493bf61295b5a55d5d6bb95eb06763fcd8e7b74e
LêMinhHuy-2280601152 <121075876+Minh-Huyy@users.noreply.github.com>
2025-04-01 11:19:37 +0700

    Add files via upload

--- lab-05/Image_Hidden/encrypt.py
+++ lab-05/Image_Hidden/encrypt.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image 
 import sys
 
 # Trong folder "img-hidden" tạo file "encrypt.py".
---
+++ lab-05/SSL/client.py
@@ -0,0 +1,48 @@
+import socket
+import ssl
+import threading
+
+# Thông tin server
+server_address = ('localhost', 123477)
+
+def receive_data(ssl_socket):
+    try:
+        while True:
+            data = ssl_socket.recv(1024)
+            if not data:
+                break
+            print("Nhận:", data.decode('utf-8'))
+    except:
+        pass
+    finally:
+        ssl_socket.close()
+        print("Kết nối đã đóng.")
+
+# Tạo socket client
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Tạo SSL context
+context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+context.verify_mode = ssl.CERT_NONE  # Thay đổi theo nhu cầu của bạn
+context.check_hostname = False  # Thay đổi theo nhu cầu của bạn
+
+# Thiết lập kết nối SSL
+ssl_socket = context.wrap_socket(client_socket, server_hostname='localhost')
+
+ssl_socket.connect(server_address)
+
+# Bắt đầu luồng để nhận dữ liệu từ server
+receive_thread = threading.Thread(target=receive_data, args=(ssl_socket,))
+receive_thread.start()
+
+
+try:
+    while True:
+        message = input("Nhập thông điệp để gửi: ")
+        ssl_socket.send(message.encode('utf-8'))
+except KeyboardInterrupt:
+    pass
+finally:
+    ssl_socket.close()
+    
+
